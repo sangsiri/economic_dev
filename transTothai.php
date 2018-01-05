771da108ede4e7c3e0c5bd0ea7acf0d1641e4760
ppirch <pakinpert@windowslive.com>
2018-01-06 01:59:06 +0700

    eng -< thai

---
+++ transTothai.php
@@ -0,0 +1,8 @@
+<?php
+ require_once "GoogleTranslate.php";
+ $word = $_REQUEST['word'];
+$GT = NEW GoogleTranslate();
+$response = $GT->translate('en','th',$word); 
+//echo "<pre>";
+echo $word."   =   ".$response;
+?>

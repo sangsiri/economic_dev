2f371c8a8d9fc19d2cf64f58ec1ef7af9e980285
ppirch <pakinpert@windowslive.com>
2018-01-06 19:48:46 +0700

    Customer bot

--- CUSbot.php
+++ CUSbot.php
@@ -5,14 +5,21 @@
 
     $bot = new BOT_API($channelSecret, $access_token);
     $mess= $_REQUEST['mess'];
+
+    if($mess==""){
+        echo 'กรุณากรอกข้อความด้วย' ;
+        exit();
+    }
     $bot->sendMessageNew($user_id,  $mess);
+
     if ($bot->isSuccess()) {
         echo 'ส่งข้อความเรียบร้อยแล้ว';
         exit();
     }
+
     echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
     exit();
-    
+
     /*if (!empty($bot->isEvents)) {
         $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
         

e311b430ceea064add2cd9160b8623937b0bd7fd
ppirch <pakinpert@windowslive.com>
2018-01-06 17:28:48 +0700

    Customer bot

--- CUSbot.php
+++ CUSbot.php
@@ -1,11 +1,9 @@
 <?php
-    require_once 'CUSbotConn.php';
-    require_once '/vendor/autoload.php';
-    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-    $response = $bot->replyMessage('123456', $textMessageBuilder);
-    if ($response->isSucceeded()) {
-        echo 'Succeeded!';
-        return; 
-    }
-// Failed
-//echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
+echo "test" ;
+require_once 'CUSbotConn.php';
+require_once '\vendor\autoload.php';
+
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$response = $bot->pushMessage($user_id, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();

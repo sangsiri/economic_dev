5e529269e8b2860b548c896409c4fb16b31ce905
ppirch <pakinpert@windowslive.com>
2018-01-06 13:38:03 +0700

    Database Connection

---
+++ connDB.php
@@ -0,0 +1,18 @@
+<?php
+//mysql://bd1c9315fec3c7:f39983bf@us-cdbr-iron-east-05.cleardb.net/heroku_2de0919e7368dd7?reconnect=true
+    $url = parse_url(getenv("mysql://bd1c9315fec3c7:f39983bf@us-cdbr-iron-east-05.cleardb.net/heroku_2de0919e7368dd7?reconnect=true"));
+
+    $server = $url["us-cdbr-iron-east-05.cleardb.net"]; 
+    $username = $url["bd1c9315fec3c7"];
+    $password = $url["f39983bf"];
+    $db = substr($url["path"], 1);
+
+    $conn = new mysqli($server, $username, $password, $db);
+
+    if($conn)
+        echo "Connect Database Success" ;
+    else
+    {
+        die('Connect Error: ' . mysqli_connect_errno());
+    }
+?>

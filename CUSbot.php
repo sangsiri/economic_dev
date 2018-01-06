2f5e9f97400cc76bb410e3262d69cab03746bdb9
ppirch <pakinpert@windowslive.com>
2018-01-06 16:44:01 +0700

    Customer bot

--- CUSbot.php
+++ CUSbot.php
@@ -1,39 +1,46 @@
 <?php
     include 'CUSbotConn.php';
-    // Get POST body content
-    $content = file_get_contents('php://input');
-    // Parse JSON
-    $events = json_decode($content, true);
-    $event = $events['events'] ;
-        $mess = $_GET["mess"];
-        // Get text sent
-        // Get replyToken
-        $replyToken = $event['replyToken'];
-        // Build message to reply back
-        $messages = [
-            'type' => 'text',
-            'text' =>  $mess
-        ];
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
 
-        // Make a POST Request to Messaging API to reply to sender
-        $url = 'https://api.line.me/v2/bot/message/reply';
-        $data = [
-            'replyToken' => $replyToken,
-            'messages' => [$messages],
-        ];
-        $post = json_encode($data);
-        $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+ $mess = $_REQUEST['mess'];
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $mess;
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        $result = curl_exec($ch);
-        curl_close($ch);
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
-        echo $result . "\r\n";
-    
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-echo "OK";
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
+		}
+	}
+
+       

4c63a5d239e00253779d257f7eb9fa5772ea17fd
ppirch <pakinpert@windowslive.com>
2018-01-06 19:04:32 +0700

    Customer bot

--- CUSbot.php
+++ CUSbot.php
@@ -6,3 +6,20 @@
     echo "test";
     include 'line-bot.php';
     echo "test";
+
+    $bot = new BOT_API($channelSecret, $access_token);
+	
+    if (!empty($bot->isEvents)) {
+            
+        $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+    
+        if ($bot->isSuccess()) {
+            echo 'Succeeded!';
+            exit();
+        }
+    
+        // Failed
+        echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+        exit();
+    
+    }

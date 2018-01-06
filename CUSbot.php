12dd00afa07eb403c8a94ade4bffe03f1de4a93e
ppirch <pakinpert@windowslive.com>
2018-01-06 17:01:41 +0700

    Customer bot

--- CUSbot.php
+++ CUSbot.php
@@ -2,7 +2,7 @@
     include 'CUSbotConn.php';
     require_once '/vendor/autoload.php';
     $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
-    $response = $bot->replyMessage('<reply token>', $textMessageBuilder);
+    $response = $bot->replyMessage('123456', $textMessageBuilder);
     if ($response->isSucceeded()) {
         echo 'Succeeded!';
         return; 

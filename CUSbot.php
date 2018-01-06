323c3c46372546ef03ad8c3de8894a20fc2349ef
ppirch <pakinpert@windowslive.com>
2018-01-06 19:01:32 +0700

    Customer bot

--- CUSbot.php
+++ CUSbot.php
@@ -2,7 +2,7 @@
     echo "test";
     include 'CUSbotConn.php';
     echo "test";
-    include '/vendor/autoload.php';
+    include 'vendor/autoload.php';
     echo "test";
     include 'line-bot.php';
     echo "test";

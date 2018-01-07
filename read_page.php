5e44200a6a94e852da8672f622da149683091efe
ppirch <pakinpert@windowslive.com>
2018-01-07 12:58:32 +0700

    read from web

--- read_page.php
+++ read_page.php
@@ -11,4 +11,18 @@ curl_close($curl);
 preg_match("/<span id=\"yfs_l10_usdthb=x\".*span>/", $result, $matches1);
 print_r($matches1);
 
+
+$request = "format=csv&by=member&rs=hour&rk=productivity&rb=".$month_first_date."&re=".$cur_date = date('Y-m-d');
+$urlWithoutProtocol = "https://www.rescuetime.com/anapi/data/?".$request ;
+$isRequestHeader = FALSE;
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $urlWithoutProtocol);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$productivity = curl_exec($ch);
+
+curl_close($ch);
+print_r($productivity);
+
 ?>

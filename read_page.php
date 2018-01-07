3fefb8b93940ffdfc3db50459ed109673c6f56bb
ppirch <pakinpert@windowslive.com>
2018-01-07 13:41:13 +0700

    read from web

--- read_page.php
+++ read_page.php
@@ -1,8 +1,8 @@
 <?php
 
-/*
+
 $request = "อาหาร";
-//$urlWithoutProtocol = "https://www.priceza.com/search?productdataname=".$request ;
+$urlWithoutProtocol = "https://www.priceza.com/search?productdataname=".$request ;
 $isRequestHeader = FALSE;
  
 $ch = curl_init();
@@ -12,9 +12,8 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $productivity = curl_exec($ch);
 curl_close($ch);
 
-*/
-$xml = '<h3 itemprop="name">Homemate เครื่องผสม<b>อาหาร</b> รุ่น HOM-150123</h3>';
+//$xml = '<h3 itemprop="name">Homemate เครื่องผสม<b>อาหาร</b> รุ่น HOM-150123</h3>';
 //preg_match("/<span id=\"yfs_l10_usdthb=x\".*span>/", $productivity, $name);
-preg_match('/<h3 itemprop="name">(.+?)<\/h3>/', $xml, $name);
+preg_match('/<h3 itemprop="name">(.+?)<\/h3>/', $productivity, $name);
 echo html_entity_decode($name[1]);
 ?>

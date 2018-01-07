cf61daa620d533df333cbdcddd95ff412f0884d9
ppirch <pakinpert@windowslive.com>
2018-01-07 13:19:05 +0700

    read from web

--- read_page.php
+++ read_page.php
@@ -11,7 +11,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $productivity = curl_exec($ch);
 
 //preg_match("/<span id=\"yfs_l10_usdthb=x\".*span>/", $productivity, $name);
-preg_match("/<div class=\"info-detail visible-xs\".*</div>/", $productivity, $name);
+preg_match('/<h3 itemprop="name">(.+?)</h3>/', $productivity, $name);
 curl_close($ch);
 print_r($name);
 

852d9525df05319713bebf3f26689a6a5f1bf011
ppirch <pakinpert@windowslive.com>
2018-01-07 15:50:12 +0700

    read from web

--- read_page.php
+++ read_page.php
@@ -1,5 +1,34 @@
 <?php
 
+function getSubStrTH($string, $start, $length)
+{			
+	$length = ($length+$start)-1;
+	$array = getMBStrSplit($string);
+	$count = 0;
+	$return = "";
+		
+	for($i=$start; $i < count($array); $i++)
+	{
+		$ascii = ord(iconv("UTF-8", "TIS-620", $array[$i] ));
+		
+		if( $ascii == 209 ||  ($ascii >= 212 && $ascii <= 218 ) || ($ascii >= 231 && $ascii <= 238 ) )
+		{
+			//$start++;
+			$length++;
+		}
+		
+		if( $i >= $start )
+		{
+			$return .= $array[$i];
+		}
+		
+		if( $i >= $length )
+			break;
+		}
+	
+	return $return;
+}
+
     $request = $_REQUEST['search'];
     $urlWithoutProtocol = "https://www.priceza.com/search?productdataname=".$request ;
     $isRequestHeader = FALSE;
@@ -19,7 +48,7 @@
 
     foreach ($name as $val) {
         $text = str_replace($search, '',$val[1]);
-        $temp = $temp.substr($text, 0, 100).(strlen($text) > 100 ? "..." : '').'\n';
+        $temp = $temp.(getStrLenTH($text) > 50) ? getSubStrTH($text, 0, 50). "..." : $text . '\n' ;
         $count++;
         if($count > 5)
         break;

b368e1aae63824c7fa7490703b84c044106db46d
ppirch <pakinpert@windowslive.com>
2018-01-07 11:39:17 +0700

    Edit text

--- help.php
+++ help.php
@@ -1,5 +1,5 @@
 <?php
 
-    echo 'ท่านสามารถใช้บริการได้ด้วยการพิมพ์\n  1. ยอดรายจ่าย\n  2. ยอดรายรับ\n  3. แนะนำการขาย \nอ่านเพิ่มเติมhttps://economic-dev.herokuapp.com/help_page.php' ;
+    echo 'ท่านสามารถใช้บริการได้ด้วยการพิมพ์\n  1. ขายของไม่ออก\n  2. ขายอะไรดี\n  3. เทคนิคการขาย\nอ่านเพิ่มเติมhttps://economic-dev.herokuapp.com/help_page.php' ;
 
 ?>

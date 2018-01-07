4b6194d532bd958b0d444dac2cdd40d810bc8b89
ppirch <pakinpert@windowslive.com>
2018-01-07 14:57:32 +0700

    read from web

--- read_page.php
+++ read_page.php
@@ -12,8 +12,12 @@
     curl_close($ch);
     
     preg_match_all('/<h3 itemprop="name">(.+?)<\/h3>/', $productivity, $name, PREG_SET_ORDER);
+    $count = 1 ;
     foreach ($name as $val) {
-        echo "find: " . $val[1] . "\n";
+        echo $val[1]."\n";
+        $count++;
+        if($count > 5)
+        break;
     }
 // The \\2 is an example of backreferencing. This tells pcre that
 // it must match the second set of parentheses in the regular expression

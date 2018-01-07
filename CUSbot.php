75eab625cd147253eec00451237b8ce1a7300d89
ppirch <pakinpert@windowslive.com>
2018-01-07 19:52:06 +0700

    Add reply

--- CUSbot.php
+++ CUSbot.php
@@ -16,8 +16,16 @@
     echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
     exit();
 
-    /*if (!empty($bot->isEvents)) {
+    if (!empty($bot->isEvents)) {
+		
         $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
-        
     
-    }*/
+        if ($bot->isSuccess()) {
+            echo 'Succeeded!';
+            exit();
+        }
+    
+        echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
+        exit();
+    
+    }

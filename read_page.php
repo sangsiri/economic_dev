b2c25d31bb35f304e9ace94a679f9bdf72c465d6
ppirch <pakinpert@windowslive.com>
2018-01-07 12:55:49 +0700

    read from web

--- read_page.php
+++ read_page.php
@@ -1,12 +1,14 @@
 <?php
-$request = "format=csv&by=member&rs=hour&rk=productivity&rb=".$month_first_date."&re=".$cur_date = date('Y-m-d');
-$urlWithoutProtocol = "https://www.rescuetime.com/anapi/data/?".$request ;
-$isRequestHeader = FALSE;
- 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $urlWithoutProtocol);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$productivity = curl_exec($ch);
-curl_close($ch);
+
+$curl = curl_init('http://finance.yahoo.com/q?s=USDTHB=X'); 
+curl_setopt($curl, CURLOPT_FAILONERROR, true); 
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true); 
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); 
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);   
+$result = curl_exec($curl); 
+curl_close($curl);
+preg_match("/<span id=\"yfs_l10_usdthb=x\".*span>/", $result, $matches1);
+print_r($matches1);
+
 ?>

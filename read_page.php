ebdb892e43472f8498ef84530c4e8283b34cccfd
ppirch <pakinpert@windowslive.com>
2018-01-07 13:02:10 +0700

    read from web

--- read_page.php
+++ read_page.php
@@ -1,19 +1,7 @@
 <?php
 
-$curl = curl_init('http://finance.yahoo.com/q?s=USDTHB=X'); 
-curl_setopt($curl, CURLOPT_FAILONERROR, true); 
-curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true); 
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); 
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false); 
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);   
-$result = curl_exec($curl); 
-curl_close($curl);
-preg_match("/<span id=\"yfs_l10_usdthb=x\".*span>/", $result, $matches1);
-print_r($matches1);
-
-
-$request = "format=csv&by=member&rs=hour&rk=productivity&rb=".$month_first_date."&re=".$cur_date = date('Y-m-d');
-$urlWithoutProtocol = "https://www.rescuetime.com/anapi/data/?".$request ;
+$request = "อาหาร";
+$urlWithoutProtocol = "https://www.priceza.com/search?productdataname=".$request ;
 $isRequestHeader = FALSE;
  
 $ch = curl_init();

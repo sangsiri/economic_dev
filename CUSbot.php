389c79d75f9f44133cc25df882a5a6a692d6d016
ppirch <pakinpert@windowslive.com>
2018-01-09 00:33:56 +0700

    Location add

--- CUSbot.php
+++ CUSbot.php
@@ -14,13 +14,13 @@
     }
 
     echo $bota->response->getHTTPStatus . ' ' . $bota->response->getRawBody(); 
-
+?>
     use Monolog\Logger;
     use Monolog\Handler\StreamHandler;
     use Monolog\Handler\FirePHPHandler;
     $logger = new Logger('LineBot');
     $logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["M0NEBw2f/MCeV4SZeh+Y/puCjXg/nGSyM86bfdXM/z1vIN8ai5iv0Lc+diByetKFacHgbDzmcXWS1l2MZl/EFl34w6UGXThdtwENFKRvVtvU9D29L4KS5GQub7P2B/vMFBywI/wW9+frlh73wbfsoAdB04t89/1O/w1cDnyilFU="]);
+    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
     $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["c24368465222a40283f89553c3bcac02"]]);
     $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
     try {

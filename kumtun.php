7c47e569089865e7c44dffd0f3c8639f0c4765e4
ppirch <pakinpert@windowslive.com>
2018-02-13 22:38:51 +0700

    test

--- kumtun.php
+++ kumtun.php
@@ -3,6 +3,7 @@
 $gets  =  $_REQUEST['temp'];
 $input =explode("-", $gets);
 $temp = 500 /($input[1]-$input[0] ) ;
+$temp = number_format($temp, 2, '.', '');
 echo "คุณจะคุ้มทุนเมื่อขาย".$temp."ชิ้นที่ราคา".$input[1]."บาทต่อชิ้น"."เมื่อต้นทุนคงที่ทั้งหมด"."500"."บาท" ;
 
 ?>
